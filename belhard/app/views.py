7539afc8103dce9320e663f596cf8598aa624645
ArtsemDev <thommy.python.dev@gmail.com>
2022-10-08 10:24:12 +0300

    optimize imports

--- belhard/app/views.py
+++ belhard/app/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from .models import Category, Product
-
 
 def index(request: HttpRequest):
     return render(request, 'app/index.html')
